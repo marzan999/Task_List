9ecabf896eda59f8552897798104bfb36aef8835
Marzan Islam <marzanislam999@gmail.com>
2022-10-02 00:44:00 +0600

    .

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last
 
 import 'package:flutter/material.dart';
 
@@ -46,7 +46,7 @@ class HomePage extends StatelessWidget {
               height: 10,
             ),
             Text(
-              '6 Tasks are remaining',
+              '6 Tasks are pending',
               style: TextStyle(
                   fontSize: 20,
                   fontWeight: FontWeight.normal,
@@ -77,7 +77,7 @@ class HomePage extends StatelessWidget {
                     height: 10,
                   ),
                   Text(
-                    'Flutter',
+                    'Alex and Jesia',
                     style: TextStyle(
                         fontSize: 20,
                         fontWeight: FontWeight.normal,
@@ -87,19 +87,186 @@ class HomePage extends StatelessWidget {
                     height: 10,
                   ),
                   Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      CircleAvatar(
-                        backgroundImage: NetworkImage(
-                            'https://img.freepik.com/premium-photo/young-handsome-man-with-beard-isolated-keeping-arms-crossed-frontal-position_1368-132662.jpg?w=2000'),
+                      Row(
+                        children: [
+                          CircleAvatar(
+                            backgroundImage: NetworkImage(
+                                'https://t4.ftcdn.net/jpg/02/97/24/51/360_F_297245133_gBPfK0h10UM3y7vfoEiBC3ZXt559KZar.jpg'),
+                          ),
+                          CircleAvatar(
+                            backgroundImage: NetworkImage(
+                                'https://img.freepik.com/free-photo/pretty-smiling-joyfully-female-with-fair-hair-dressed-casually-looking-with-satisfaction_176420-15187.jpg?w=2000'),
+                          ),
+                        ],
                       ),
-                      CircleAvatar(
-                        backgroundImage: NetworkImage(
-                            'https://img.freepik.com/premium-photo/young-handsome-man-with-beard-isolated-keeping-arms-crossed-frontal-position_1368-132662.jpg?w=2000'),
-                      ),
+                      Row(
+                        children: [
+                          Text(
+                            'Now',
+                            style: TextStyle(fontSize: 15, color: Colors.white),
+                          ),
+                        ],
+                      )
                     ],
-                  )
+                  ),
                 ],
               ),
+            ),
+            SizedBox(
+              height: 35,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Monthly Review',
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                ),
+                Icon(
+                  Icons.calendar_month,
+                  color: Color.fromARGB(255, 42, 194, 110),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(35, 30, 10, 10),
+                        child: ListTile(
+                          title: Text(
+                            '22',
+                            style: TextStyle(
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                          subtitle: Text(
+                            'Done',
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.normal,
+                                color: Colors.white),
+                          ),
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Color.fromARGB(255, 81, 142, 199),
+                      ),
+                      height: 150,
+                      width: 150,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(30, 10, 10, 10),
+                        child: ListTile(
+                          title: Text(
+                            '10',
+                            style: TextStyle(
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                          subtitle: Text(
+                            'Ongoing',
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.normal,
+                                color: Colors.white),
+                          ),
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Color.fromARGB(255, 81, 142, 199),
+                      ),
+                      height: 100,
+                      width: 150,
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Column(
+                      children: [
+                        Container(
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(15, 10, 10, 10),
+                            child: ListTile(
+                              title: Text(
+                                '7',
+                                style: TextStyle(
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              ),
+                              subtitle: Text(
+                                'In Progress',
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.normal,
+                                    color: Colors.white),
+                              ),
+                            ),
+                          ),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Color.fromARGB(255, 81, 142, 199),
+                          ),
+                          height: 100,
+                          width: 150,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(20, 25, 12, 10),
+                            child: ListTile(
+                              title: Text(
+                                '12',
+                                style: TextStyle(
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              ),
+                              subtitle: Text(
+                                'Nothing for Review',
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.normal,
+                                    color: Colors.white),
+                              ),
+                            ),
+                          ),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Color.fromARGB(255, 81, 142, 199),
+                          ),
+                          height: 150,
+                          width: 150,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ],
             ),
 
             //end
@@ -109,3 +276,52 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+
+
+
+//  Column(
+//               children: [
+//                 Row(
+//                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                   children: [
+//                     Container(
+//                       decoration: BoxDecoration(
+//                         borderRadius: BorderRadius.circular(20),
+//                         color: Colors.white,
+//                       ),
+//                       height: 150,
+//                       width: 150,
+//                     ),
+//                     Container(
+//                       decoration: BoxDecoration(
+//                         borderRadius: BorderRadius.circular(20),
+//                         color: Colors.white,
+//                       ),
+//                       height: 100,
+//                       width: 150,
+//                     ),
+//                   ],
+//                 ),
+//                 Row(
+//                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                   children: [
+//                     Container(
+//                       decoration: BoxDecoration(
+//                         borderRadius: BorderRadius.circular(20),
+//                         color: Colors.white,
+//                       ),
+//                       height: 100,
+//                       width: 150,
+//                     ),
+//                     Container(
+//                       decoration: BoxDecoration(
+//                         borderRadius: BorderRadius.circular(20),
+//                         color: Colors.white,
+//                       ),
+//                       height: 150,
+//                       width: 150,
+//                     ),
+//                   ],
+//                 )
+//               ],
+//             )
